aaf052116d7d1f18b7b1ea38a8010a19300e3a6a
Eyal Lewin <eyal@the5ers.com>
2024-09-22 22:13:39 +0300

    initial page

--- apps/assignment-boilerplate/src/app/app.tsx
+++ apps/assignment-boilerplate/src/app/app.tsx
@@ -6,7 +6,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
-      <NxWelcome title="assignment-boilerplate" />
+      <NxWelcome title="5% assignment" />
     </div>
   );
 }
